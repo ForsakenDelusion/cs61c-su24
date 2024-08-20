01ff3e126b3d648239720a02bdc659a5e6909874
ForsakenDelusion <wangcy0205@outlook.com>
2024-08-20 12:53:49 +0800

    finishi ex3

--- lab04/ex3.s
+++ lab04/ex3.s
@@ -32,7 +32,8 @@ main:
 #     where ^ is the exponent operator, not XOR
 ex3:
     # Note: Add code BELOW without altering existing lines.
-
+    addi sp sp -4
+    sw ra 0(sp)
     # return 1 if a1 == 0
     beq a1 x0 ex3_zero_case
 
@@ -53,4 +54,6 @@ ex3_zero_case:
     li a0 1
 
 ex3_end:
+    lw ra 0(sp)
+    addi sp sp 4
     jr ra
